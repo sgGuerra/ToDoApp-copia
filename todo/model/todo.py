2c513bd0263a5bf395aaf9f1dd1e8895467369c3
Luis Guerra <guerraherrera000@gmail.com>
2024-08-26 12:25:55 -0500

    primer commit, creación clase Todo

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -1,1 +1,9 @@
-# TODO: Add code here
+class Todo:
+    def __init__(self, code: int, title: str, description: str, completed: False, tag: list[str]):
+        self.code: int = code
+        self.title: str = title
+        self.description: str = description
+        self.completed: False = completed
+        self.tag: list[str] = tag
+
+
