9d8ab736237710858071d217d8093c991bfa0749
Luis Guerra <guerraherrera000@gmail.com>
2024-08-26 13:45:48 -0500

    creación clase TodoBook

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -1,15 +1,15 @@
 class Todo:
-    def __init__(self, code_id: int, title: str, description: str, completed: False, tags: list[str]):
+    def __init__(self, code_id: int, title: str, description: str):
         self.code_id: int = code_id
         self.title: str = title
         self.description: str = description
-        self.completed: False = completed
-        self.tags: list[str] = tags
+        self.completed: bool = False
+        self.tags: list[str] = []
 
     def mark_completed(self):
         self.completed = True
 
-    def add_tag(self, tag) -> str | list:
+    def add_tag(self, tag) -> str | list[str]:
         if tag in self.tags:
             return f"el elemteno {tag} ya esta exite en la lista"
 
@@ -17,7 +17,21 @@ class Todo:
             self.tags.append(tag)
 
     def __str__(self) -> str:
-        return f"{self.code_id}-{self.title}"
+        return f"{self.code_id} - {self.title}"
+
+
+class TodoBook:
+
+    def __init__(self):
+        self.todos: dict[int, Todo] = {}
+
+
+
+
+
+
+
+
 
 
 
