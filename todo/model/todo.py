a1c0eba653ac0fc3539abd4c33907c9efb1f26ee
Luis Guerra <guerraherrera000@gmail.com>
2024-08-26 12:42:29 -0500

    creacion de los metodos mark_completed, add_tag, __str__

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -1,9 +1,23 @@
 class Todo:
-    def __init__(self, code: int, title: str, description: str, completed: False, tag: list[str]):
-        self.code: int = code
+    def __init__(self, code_id: int, title: str, description: str, completed: False, tags: list[str]):
+        self.code_id: int = code_id
         self.title: str = title
         self.description: str = description
         self.completed: False = completed
-        self.tag: list[str] = tag
+        self.tags: list[str] = tags
+
+    def mark_completed(self):
+        self.completed = True
+
+    def add_tag(self, tag) -> str | list:
+        if tag in self.tags:
+            return f"el elemteno {tag} ya esta exite en la lista"
+
+        else:
+            self.tags.append(tag)
+
+    def __str__(self) -> str:
+        return f"{self.code_id}-{self.title}"
+
 
 
